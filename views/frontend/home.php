abc36565b0aaeebef28f106bdef34d29751597d3
KaYeYe <ridwanauliansyach@gmail.com>
2026-08-17 18:50:42 +0700

    feat: variasi warna thumbnail fallback card berita beranda

--- views/frontend/home.php
+++ views/frontend/home.php
@@ -31,7 +31,7 @@
                 </div>
 
                 <?php if (!empty($latestNews)): ?>
-                    <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="row row-cols-1 row-cols-md-2 g-4 news-grid">
                         <?php foreach ($latestNews as $item): ?>
                             <div class="col">
                                 <div class="card h-100">
